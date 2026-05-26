8598e32ad1b2e26143d4561c056c015cfe8d9c62
czkevwalk-ux <czkevwalk@gmail.com>
2026-05-26 16:33:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // =====================================================
-// 💾 CACHE EN MEMORIA RAM
+// 💾 CACHE EN MEMORIA RAM1
 // ====================================================
 const CACHE_LIMIT = 800;
 let cache = [];
